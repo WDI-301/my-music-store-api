e292a73ff2c25df640b6ef0081ff8127b6302630
Omar Suriel <omarsuriel@icloud.com>
2022-03-03 15:17:07 -0500

    Products can be uploaded to server and fetched

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 const userRouter = require('./routes/userRouter');
 var cors = require('cors');
+const productRouter = require('./routes/productRouter');
 
 mongoose.connect(process.env.DB_CONNECTION_STRING)
 .then(() => console.log('connected to databasew successfully'))
@@ -15,7 +16,9 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
+// Attach routers to our app
 app.use(userRouter);
+app.use(productRouter);
 
 app.listen(5100, () => {
   console.log('MyMusicStore API server is listening on port 5100')
