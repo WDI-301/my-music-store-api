69ea24b85324cd25fb7b5ca503a45566f0ec4b44
Omar Suriel <omarsuriel@icloud.com>
2022-03-07 12:32:50 -0500

    making product router for admins only 1st iteration no tokens or checkign db

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -6,6 +6,20 @@ const productRouter = express.Router();
 // Store the the products to the database
 productRouter.post('/upload-product', async (req, res) => {
 
+  // see if the user making the request is an admin
+    // See who is the user making the request
+    const user = req.body.user;
+
+
+    // Once we know who the user is, lets check if he/she is an admin
+      // BUT WE DONT KNOW WHO THE USER IS.
+      // Just because the request says someone is an user, it doesnt mean they are.
+
+    // if theyre not and admin throw some error telling them they cant hit this route
+    if(!user.isAdmin){
+      throw new Error('User is not an admin!!')
+    }
+
   // Grab the product data from the request
   const productData = req.body.productData;
 
